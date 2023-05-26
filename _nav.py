d37ab58e70b63053f00b00a4287826776bce685e
Tim Menzies <timm@ieee.org>
2023-05-26 14:31:01 -0400

    as

--- _nav.py
+++ _nav.py
@@ -1,8 +1,12 @@
 def content(name="Tim Menzies",email="timm@ieeee.org"):
   return f"""
 <img align=right height="60" src="https://brand.ncsu.edu/img/logo/brick2x2.jpg">
-<p>{name} ::  <a href="home.html">home</a> 
-:: <a href="mailto:{name}">{email}</a><br>
+<p>{name}:  <a href="home.html">home</a> 
+| <a href="https://arxiv.org/search/?searchtype=all&query=tim+menzies&size=200&order=-announced_date_first">pre-prints</a>
+| <a href="https://scholar.google.com/citations?user=7htTUTgmLtUC&hl=en&authuser=1">papers</a>
+| <a href="https://www.youtube.com/playlist?list=PLOgGLL2KRJ7x1qbFQjzDn1tCjCHAw6pGo">videos</a>
+| <a href="mailto:{name}">{email}</a>
+<br>
 <b>Editor-in-chief</b>, Automated SE journal<br>
 IEEE Fellow, prof, cs, usa (se, ai, re, analytics, optimization)</p>
 <hr>
--- _template.py
+++ _template.py
@@ -13,6 +13,8 @@ def content(title="",author="",description="",css="style.css",
     <meta name="author" content="{author}">
     <link rel="shortcut icon" href="{favicon}">
     <link rel="stylesheet" href="normalize.css">
+    <link href='https://fonts.googleapis.com/css?family=EB Garamond' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Dejavu Sans' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{css}">
