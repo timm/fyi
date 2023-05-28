7fc7cc5046baa13b6209ec3b65be44f5ee131017
Tim Menzies <timm@ieee.org>
2023-05-28 08:56:03 -0400

    saving

--- _nav.py
+++ _nav.py
@@ -3,14 +3,16 @@ def content(name="Tim Menzies",email="timm@ieeee.org"):
 <img align=right xstyle="padding-right: 10px;" height="60" src="https://brand.ncsu.edu/img/logo/brick2x2.jpg">
 <p >
 <a href="mailto:{name}">{email}</a>
-| <a href="https://arxiv.org/search/?searchtype=all&query=tim+menzies&size=200&order=-announced_date_first">pre-prints</a>
-| <a href="https://scholar.google.com/citations?user=7htTUTgmLtUC&hl=en&authuser=1">papers</a>
-| <a href="https://www.youtube.com/playlist?list=PLOgGLL2KRJ7x1qbFQjzDn1tCjCHAw6pGo">videos</a>
-| <a href="https://scholar.google.com/citations?user=7htTUTgmLtUC&hl=en&authuser=1"><i class="ai ai-google-scholar ai-lg"></i></a>
- &nbsp; 
+| <a href="https://arxiv.org/search/?searchtype=all&query=tim+menzies&size=200&order=-announced_date_first">pre-prints
+<i class="ai ai-arxiv ai-lg"></i>
+</a>
+| <a href="https://scholar.google.com/citations?user=7htTUTgmLtUC&hl=en&authuser=1">papers
+<i class="ai ai-google-scholar ai-lg"></i>
+</a>
+| <a href="https://www.youtube.com/playlist?list=PLOgGLL2KRJ7x1qbFQjzDn1tCjCHAw6pGo">videos
+<i class="fab fa-youtune "></i></a> 
+&nbsp; 
 <a href="https://dblp.org/pid/m/TimMenzies.html"><i class="ai ai-dblp ai-lg"></i></a>
- &nbsp; 
-<a href="https://arxiv.org/search/?query=tim+menzies&searchtype=all&source=header"><i class="ai ai-arxiv ai-lg"></i></a>
  &nbsp; 
 <a href="https://www.goodreads.com/review/list/1469588-tim-menzies?ref=nav_mybooks"><i class="fab fa-goodreads "></i></a>
  &nbsp; 
