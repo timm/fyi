27946116e110f8a6f1825e799a9d61107c550470
Tim Menzies <timm@ieee.org>
2023-05-27 20:10:13 -0400

    saving

--- _nav.py
+++ _nav.py
@@ -1,8 +1,9 @@
 def content(name="Tim Menzies",email="timm@ieeee.org"):
   return f"""
 <img align=right height="60" src="https://brand.ncsu.edu/img/logo/brick2x2.jpg">
-<p>{name}:  <a href="home.html">home</a> 
-| <a href="https://arxiv.org/search/?searchtype=all&query=tim+menzies&size=200&order=-announced_date_first">pre-prints</a>
+<p>
+{name}: 
+<a href="https://arxiv.org/search/?searchtype=all&query=tim+menzies&size=200&order=-announced_date_first">pre-prints</a>
 | <a href="https://scholar.google.com/citations?user=7htTUTgmLtUC&hl=en&authuser=1">papers</a>
 | <a href="https://www.youtube.com/playlist?list=PLOgGLL2KRJ7x1qbFQjzDn1tCjCHAw6pGo">videos</a>
 | <a href="mailto:{name}">{email}</a>
